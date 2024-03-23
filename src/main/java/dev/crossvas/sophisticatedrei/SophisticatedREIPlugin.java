7f12e583159aabfcd0c7bfb325cb32f5cbf5f662
CrossVas <cross.wanted.1.4@gmail.com>
2024-03-23 09:16:40 +0900

    add support for sophisticated storage

--- src/main/java/dev/crossvas/sophisticatedrei/SophisticatedREIPlugin.java
+++ src/main/java/dev/crossvas/sophisticatedrei/SophisticatedREIPlugin.java
@@ -9,13 +9,15 @@ import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
 import me.shedaniel.rei.forge.REIPluginClient;
-import net.p3pp3rf1y.sophisticatedbackpacks.client.gui.BackpackScreen;
-import net.p3pp3rf1y.sophisticatedbackpacks.client.gui.BackpackSettingsScreen;
+import net.minecraftforge.fml.ModList;
 
 import java.util.List;
 
 @REIPluginClient
 public class SophisticatedREIPlugin implements REIClientPlugin {
+
+    public final boolean BACKPACKS = ModList.get().isLoaded("sophisticatedbackpacks");
+    public static final boolean STORAGE = ModList.get().isLoaded("sophisticatedstorage");
 
     @Override
     public void registerTransferHandlers(TransferHandlerRegistry registry) {
@@ -24,7 +26,17 @@ public class SophisticatedREIPlugin implements REIClientPlugin {
 
     @Override
     public void registerExclusionZones(ExclusionZones zones) {
-        zones.register(BackpackScreen.class, gui -> {
+        if (STORAGE) {
+            registerStorageZones(zones);
+        }
+        if (BACKPACKS) {
+            registerBackpackZones(zones);
+        }
+
+    }
+
+    public void registerBackpackZones(ExclusionZones zones) {
+        zones.register(net.p3pp3rf1y.sophisticatedbackpacks.client.gui.BackpackScreen.class, gui -> {
             List<Rectangle> ret = new ObjectArrayList<>();
             gui.getUpgradeSlotsRectangle().ifPresent(rect2i -> ret.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight())));
             List<Rectangle> rects = new ObjectArrayList<>();
@@ -36,7 +48,29 @@ public class SophisticatedREIPlugin implements REIClientPlugin {
             return ret;
         });
 
-        zones.register(BackpackSettingsScreen.class, gui -> {
+        zones.register(net.p3pp3rf1y.sophisticatedbackpacks.client.gui.BackpackSettingsScreen.class, gui -> {
+            List<Rectangle> rects = new ObjectArrayList<>();
+            gui.getSettingsTabControl().getTabRectangles().forEach(
+                    rect2i -> rects.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight()))
+            );
+            return rects;
+        });
+    }
+
+    public void registerStorageZones(ExclusionZones zones) {
+        zones.register(net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageScreen.class, gui -> {
+            List<Rectangle> ret = new ObjectArrayList<>();
+            gui.getUpgradeSlotsRectangle().ifPresent(rect2i -> ret.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight())));
+            List<Rectangle> rects = new ObjectArrayList<>();
+            gui.getUpgradeSettingsControl().getTabRectangles().forEach(rect2i -> {
+                rects.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight()));
+            });
+            ret.addAll(rects);
+            gui.getSortButtonsRectangle().ifPresent(rect2i -> ret.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight())));
+            return ret;
+        });
+
+        zones.register(net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageSettingsScreen.class, gui -> {
             List<Rectangle> rects = new ObjectArrayList<>();
             gui.getSettingsTabControl().getTabRectangles().forEach(
                     rect2i -> rects.add(new Rectangle(rect2i.getX(), rect2i.getY(), rect2i.getWidth(), rect2i.getHeight()))
